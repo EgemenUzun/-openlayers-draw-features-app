a986b737e819ea60668418abb38ed0ae3d10990c
Egemen UZUN <avniegemen1243@gmail.com>
2024-02-06 23:00:02 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+import Draw from 'ol/interaction/Draw.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {createRegularPolygon} from 'ol/interaction/Draw';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const vector = new VectorLayer({
+  source: source,
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+});
+
+const typeSelect = document.getElementById('type');
+
+let draw; // global so we can remove it later
+function addInteraction() {
+  let value = typeSelect.value;
+  if (value !== 'None') {
+    if (value === 'Square') {
+      draw = new Draw({
+        source: source,
+        type: 'Circle',
+        geometryFunction: function(coordinates, geometry) {
+          if (!geometry) {
+            geometry = fromCircle(new ol.geom.Circle(coordinates, 5000), 64);
+          }
+          return geometry;
+        },
+      });
+    } else {
+      draw = new Draw({
+        source: source,
+        type: value,
+      });
+    }
+    map.addInteraction(draw);
+  }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function () {
+  draw.removeLastPoint();
+});
+
+addInteraction();
